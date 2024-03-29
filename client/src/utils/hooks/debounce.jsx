6d62baaaa741b873020f6b44753c319df7bd8dc2
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-03-29 19:30:58 -0400

    SCRUM-9-test-api

---
+++ client/src/utils/hooks/debounce.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react"
+export function active_debounce(textoBusq, retraso = 1000) {
+    const [debounceValor, setDebounceValor] = useState(textoBusq);
+    useEffect(() => {
+        const actTexto = setTimeout(() => {
+            //console.debug('Recibiendo', textoBusq)
+            setDebounceValor(textoBusq);//Actualizar valor
+        }, retraso);
+        return () => {
+            clearTimeout(actTexto);
+        };
+    }, [textoBusq, retraso]);//Recibir el texto del usuario y el tiempo de espera
+
+    return debounceValor;//Enviar el último valor
+};
