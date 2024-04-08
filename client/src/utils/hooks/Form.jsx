e6757bff65b8ba9933e0e06cf1745ead51a8ddc3
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-04-08 06:02:21 -0400

    SCRUM-21 adelantar formulario

--- client/src/utils/hooks/Form.jsx
+++ client/src/utils/hooks/Form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from 'react';
 import Axios from "axios";
+import logo_contact from "../../assets//logo_contact.png"
 
 function Form (){
 
@@ -37,74 +38,92 @@ function Form (){
   
   return(
     <div className="container_form">
+      <div className="container_form_img"> 
+        <img src={logo_contact} ></img>
+      </div>
       <form onSubmit={registerReport} className="form_contact">
-        <label htmlFor="name" name="name">Su nombre completo</label>
         <input 
           type="text" id="name" 
           onChange={(e)=>{setName(e.target.value)}}
+          placeholder="Nombre Completo"
+          className="input_form_contact input_contact"
         />  
 
-        <label htmlFor="reported_name" name="reported_name">Nombre de la persona que reconoció</label>
-        <input 
-          type="text" id="reported_name" 
-          onChange={(e)=>{setReportedName(e.target.value)}}
-        />   
-
-        <label htmlFor="email" name="email">Email</label>
         <input 
           type="email" 
           id="email" 
           onChange={(e)=>{setEmail(e.target.value)}}
+          placeholder="Email"
+          className="input_form_contact input_contact"
         />   
 
-        <label htmlFor="date_sighting" name="date_sighting">Fecha del avistamiento más reciente</label>
-        <input 
-          type="date" 
-          id="date_sighting"
-          onChange={(e)=>{setDateSighting(e.target.value)}} 
-        />  
-
-        <label htmlFor="phone" name="phone" >Teléfono</label>
         <input 
           type="tel" 
           id="phone" 
           onChange={(e)=>{setPhone(e.target.value)}}
+          placeholder="Teléfono"
+          className="input_form_contact input_contact"
         /> 
 
-        <label htmlFor="reported">Reportado</label>
         <input 
-          type="radio" 
-          id="reported" 
-          name="state" 
-          value="Reportado"
-          onClick={(e)=>{setState(e.target.value)}}
-        />
-        <label htmlFor="missing">Desaparecido</label>  
-        <input 
-          type="radio" 
-          id="missing" 
-          name="state" 
-          value="Desaparecido" 
-          onClick={(e)=>{setState(e.target.value)}}
-        />  
+          type="text" id="reported_name" 
+          onChange={(e)=>{setReportedName(e.target.value)}}
+          placeholder="Nombre del Reportado"
+          className="input_form_contact input_contact"
+        />   
 
-        <label htmlFor="description_report" name="description">Detalles del avistamiento</label>
+        <div>
+          <label htmlFor="date_sighting" name="date_sighting" className="label_contact_date">Avistamiento</label> 
+            <input 
+              type="date" 
+              id="date_sighting"
+              onChange={(e)=>{setDateSighting(e.target.value)}}
+              className="input_contact input_date_sighting" 
+            />  
+        </div>
+
+        <div className="input_contact_radio">
+          <label htmlFor="reported" className="radio_contact label_contact_report">Reportado
+            <input 
+              type="radio" 
+              id="reported" 
+              name="state" 
+              value="Reportado"
+              onClick={(e)=>{setState(e.target.value)}}
+              className="radio_report"
+            />
+          </label>
+          <label htmlFor="missing" className="radio_contact label_contact_missing">Desaparecido 
+            <input 
+              type="radio" 
+              id="missing" 
+              name="state" 
+              value="Desaparecido" 
+              onClick={(e)=>{setState(e.target.value)}}
+              className="radio_missing"
+            />  
+          </label> 
+        </div>
+
         <textarea  
           id="description_report"
           onChange={(e)=>{setDescription(e.target.value)}}
           className="description_area"
+          placeholder="Descripción del avistamiento"
         ></textarea>
 
-        <label htmlFor="photo">Foto del avistamiento más reciente</label>
-        <input 
-          type="file" 
-          id="photo" 
-          name="photo" 
-          accept="image/*" 
-          onChange={(e)=>{setPhoto(e.target.files[0])}}
-        />  
+        <div className="input_contact">
+          <label htmlFor="photo" className="label_contact_evidence">Evidencia </label>
+          <input 
+            type="file" 
+            id="photo" 
+            name="photo" 
+            accept="image/*" 
+            onChange={(e)=>{setPhoto(e.target.files[0])}}
+          />  
+        </div>
 
-        <input type="submit" />
+        <input type="submit" className="contact_submit_button"/>
       </form>
     </div>
   );
