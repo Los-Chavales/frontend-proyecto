299071b2a22e78e3a4dab9c581ff25736ed9c815
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-04-10 19:26:40 -0400

    SCRUM-23-admin-menu-path

--- client/src/components/Table_reports.jsx
+++ client/src/components/Table_reports.jsx
@@ -3,13 +3,11 @@ import "../styles/table.css"
 
 function TableReports(report) {
     //console.log(report.title, report.data, report.columns, report.styles)
-    const columns = report.columns
-
     return (
         <div className="tables">
             <h1 className="tables-title">{report.title}</h1>
             <DataTable
-                columns={columns}
+                columns={report.columns}
                 data={report.data}
                 pagination
                 paginationPerPage={3}
--- client/src/pages/Reports_page.jsx
+++ client/src/pages/Reports_page.jsx
@@ -50,7 +50,6 @@ const tableStylesR = {
         }
     }
 }
-
 const tableStylesY = {
     headCells: {
         style: {
