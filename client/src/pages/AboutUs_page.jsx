231ad1780607007e443917aac92f489f3088fe3b
Crisangelly <hernandezfernandezcd@uvm.edu.ve>
2024-04-10 19:22:40 -0400

    SCRUM-17 cambios

--- client/src/pages/AboutUs_page.jsx
+++ client/src/pages/AboutUs_page.jsx
@@ -3,7 +3,7 @@ import LupaLogo from "../assets/lupita.png"
 function AboutUs(){
     return(
         <main className="about_us-container">
-           
+           <div className="Prueba"></div>
             <div className="aboutus-logo">
                 <div className="imagen-div">
                     <img src={LupaLogo} alt="Logo de MW Tracked" className="lupa-img" />
@@ -17,10 +17,12 @@ function AboutUs(){
                     <p>La Organización Internacional de Policía Criminal es la mayor organización de policía internacional que une a las fuerzas de seguridad de 196 países de todo el mundo</p>
                   </div>
                   <div className="info-2">
-                  Una notificación roja es un aviso internacional sobre una persona buscada,se utilizan para avisar  a los servicios policiales de todos los países miembros de la existencia de prófugos buscados a escala internacional
+                  <p>Una notificación roja es un aviso internacional sobre una persona buscada,se utilizan para avisar  a los servicios policiales de todos los países miembros de la existencia de prófugos buscados a escala internacional</p>
                   </div>
                   <div className="info-3">
+                  <p>
                   Una notificación amarilla es una alerta policial mundial sobre una persona desaparecida, se publican para ayudar a localizar a personas desaparecidas, niños, adultos y mayores que incluso son incapaces de identificarse a sí mismos
+                  </p>
                   </div>  
             </div>
             
