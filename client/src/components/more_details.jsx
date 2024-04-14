88679225de9734c2e5c816067bf2efe37bab3bc2
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-04-14 16:50:43 -0400

    SCRUM-36-corregir-estilos-detalles

--- client/src/components/more_details.jsx
+++ client/src/components/more_details.jsx
@@ -1,48 +1,54 @@
 import React, { useState } from "react";
 import "../styles/coincidence.css"
 import "../styles/bar_range.css"
-import Range from "../utils/hooks/Range"; 
-import { Link } from "react-router-dom"; 
+import Range from "../utils/hooks/Range";
+import { Link } from "react-router-dom";
 
 function MoreDetails({ values, state, closeModal, changeClose }) {
   return (
     <>
-    {closeModal &&  
-      <div className="div_detail">
-        <div className="containerCardMore redCard">
-          <div className="personCardRedMore">
-            <div className="divMore1">
-              <div className="imagenCardMoreContainer">
-                <img className="imagenCardMore" src={values.image} alt="Reportado"></img>
+      {closeModal &&
+        <div className="div_detail">
+          <div className="containerCardMore redCard">
+            <div className="personCardRedMore">
+              <div className="divMore1">
+                <div className="imagenCardMoreContainer">
+                  <img className="imagenCardMore" src={values.image} alt="Reportado"></img>
+                </div>
+                <h2 className="usernameMore"><strong>{values.forename}</strong></h2>
               </div>
-              <h2 className="usernameMore"><strong>{values.forename}</strong></h2>
-            </div>
 
-            <div className="divMore">
-            <div className="close_container">
-              <button className="close_button" onClick={() => { changeClose(false) }}>X</button>
-            </div>
-            <ul>
-              <li>Apellido: {values.name}</li>
+              <div className="divMore">
+                <div className="close_container">
+                  <button className="close_button" onClick={() => { changeClose(false) }}>X</button>
+                </div>
+                <div className="divDetails">
+                  <div className="divDetailsContents">
+                    <ul className="divDetailsContents">
+                      <li>Apellido: {values.name}</li>
 
-              <li>Fecha de nacimiento: {values.date_of_birth} </li>
-              <li>Nacionalidad: {values.nationalities}</li>
-              
-            </ul>
-            <Range arrest_details={values.arrest_details} /> 
-            <hr />
-            <Link to={`/report/${values.entity_id}/${values.forename}/${state}`} target="_blank" rel="noopener noreferrer" ><button className="more_button">Reportar</button></Link>
-            <Link to={`/coincidence_reports/${values.entity_id}`} target="_blank" rel="noopener noreferrer" ><button className="more_button">Ver Reportes</button></Link>
+                      <li>Fecha de nacimiento: {values.date_of_birth} </li>
+                      <li>Nacionalidad: {values.nationalities}</li>
+
+                    </ul>
+                    <Range arrest_details={values.arrest_details} />
+                  </div>
+                  <hr />
+                  <div>
+                    <Link to={`/report/${values.entity_id}/${values.forename}/${state}`} target="_blank" rel="noopener noreferrer" ><button className="more_button">Reportar</button></Link>
+                    <Link to={`/coincidence_reports/${values.entity_id}`} target="_blank" rel="noopener noreferrer" ><button className="more_button">Ver Reportes</button></Link>
+                  </div>
+                </div>
+              </div>
+
             </div>
 
           </div>
-
         </div>
-      </div>
-    }
-      </>
+      }
+    </>
   );
-  
+
 }
 
 export default MoreDetails;
